5670f8db3b168ee1c6615d3063012e53786e17f8
Arming <cloudniw1@126.com>
2013-10-13 10:49:44 +0800

    support pagination

--- handlers/home.py
+++ handlers/home.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python
+# encoding:utf-8
 
 from base import BaseHandler
+from pagination import Paginator
 
 
 class HomeHandler(BaseHandler):
 
     def get(self):
-        self.write("This is home page!")
+        pager = Paginator(self.db, 1)
+        # self.write("It's ok")
+        com = pager.get_company()
+        self.render(
+            "home.html", companies=com, paginator=pager)
+
